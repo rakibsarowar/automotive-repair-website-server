477ef1a55382b8afa1a359a47af8f60994faebb3
rakibsarowar <rakibsarowar@gmail.com>
2023-05-13 20:20:24 +0600

    added the send and lister

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('doctor is running')
+})
+
+app.listen(port, () => {
+    console.log(`Car Doctor Server is running on port ${port}`)
+})
