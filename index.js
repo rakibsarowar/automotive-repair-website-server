1a7f659abc2a865d6a26ec136fa377a24f803212
rakibsarowar <rakibsarowar@gmail.com>
2023-05-13 21:15:00 +0600

    added env file  but not included in gitignore

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
+
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+console.log(process.env.DB_PASS)
+console.log(process.env.DB_USER)
+
 // Code of MongoDB--------------------
-const uri = "mongodb+srv://coffeeMaster:<password>@cluster0.d94f49k.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://docUser:IjNLf7SDyL28Ootz@cluster0.d94f49k.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
